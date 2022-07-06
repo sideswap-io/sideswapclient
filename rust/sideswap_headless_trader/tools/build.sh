08f75a7cc275bf406ebec14e07e9ed3d21bee448
SideSwap <hello@sideswap.io>
2022-07-06 10:27:22 +0300

    Remove build files from the docker image

--- rust/sideswap_headless_trader/tools/build.sh
+++ rust/sideswap_headless_trader/tools/build.sh
@@ -3,9 +3,11 @@ set -e
 
 apt update -qq
 apt upgrade -yqq
-
 apt install --no-install-recommends unzip autoconf automake autotools-dev pkg-config build-essential libtool python3{,-dev,-pip,-virtualenv} python{,-dev}-is-python3 ninja-build clang{,-format,-tidy} git swig openjdk-11-jdk curl libudev-dev lld -yqq
+apt clean
+
 update-java-alternatives -s java-1.11.0-openjdk-amd64
+
 pip3 install meson==0.58.0
 
 curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain 1.60.0
@@ -31,3 +33,8 @@ popd
 pushd sideswapclient/rust/sideswap_headless/
 cargo build --release
 popd
+
+cd /
+mkdir apps
+cp /build/sideswapclient/rust/target/release/sideswap_headless /build/sideswapclient/rust/target/release/sideswap_headless_trader /apps
+rm -rf /build
